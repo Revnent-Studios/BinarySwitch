b00462da4a1ab2d82824cae90c1a8a2fd1903c55
= <shashwatthegamer332@gmail.com>
2024-08-09 00:42:22 +0530

    basic wall hang added, wall jump flag added

--- objects/Player/player.gd
+++ objects/Player/player.gd
@@ -4,10 +4,15 @@ class_name player
 
 # false is 0, true is 1
 var state = false
+var onwall = false
+
+# if true can wall jump
+var walljump = true
 
 const speed = 300
-const jump = 500
-const gravity = 1500
+const jump = 750
+const gravity = 2000
+const fallspeedcap = 750
 
 
 func _input(event):
@@ -16,10 +21,28 @@ func _input(event):
 		state= !(state)
 
 func Jump(delta):
-	velocity.y = -jump
+	if is_on_floor():
+		velocity.y = -jump
+	elif is_on_wall_only():
+		# implement wall jump
+		
+		# walljump set to false if jumped when on wall only
+		walljump = false
+
 
 func Fall(delta):
-	velocity.y += (gravity*delta)
+	#print(velocity.y)
+	if !onwall:
+		if velocity.y < fallspeedcap:
+			velocity.y += (gravity*delta)
+	else:
+		velocity.y = 0
+		
+		# i have no idea why i needed to multiply delta by 5, it's too slow if i dont
+		# removing velocity.y = 0 fixes it but we need to set velocity.y to 0 
+		
+		# make this slowly decrease until it reaches a certain threshhold
+		velocity.y += (gravity*delta*5)
 
 func playerMovement(delta):
 	var vector = getDir()	
