4869436d66a4f11295fd0b6bd8c07b09c78667e5
Pratyush-Remon <pratyushsoni1704@gmail.com>
2024-08-10 19:01:20 +0530

    completed level manager

--- objects/levelmanger/levelmanger.gd
+++ objects/levelmanger/levelmanger.gd
@@ -3,4 +3,16 @@ class_name LevelManager
 
 @onready var level = get_tree()
 
+var key
+var door
 
+func _ready():
+	key = level.get_first_node_in_group("key")
+	door = level.get_first_node_in_group("door")
+
+
+
+func _on_key_collect_key():
+	level.get_first_node_in_group("key").queue_free()
+	door.open()
+	
